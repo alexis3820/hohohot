80f437115b31d97ea3afbdd740dd91a0ee85cb96
alexi <alexis3.avet8@gmail.com>
2021-03-13 20:01:30 +0100

    search entry point

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a8c4b183262061ed2e2fceb38e74005c528b6db0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8c4b183262061ed2e2fceb38e74005c528b6db0',
+    ),
+  ),
+);
