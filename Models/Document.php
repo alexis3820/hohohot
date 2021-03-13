c5e3516661c5ae7d6ceaabc15e92411b543caad9
alexi <alexis3.avet8@gmail.com>
2021-03-13 19:36:03 +0100

    refactor

--- Models/Document.php
+++ Models/Document.php
@@ -10,7 +10,7 @@ class Document
         'odt',
     ];
 
-    private $directory = '../public/static/documentation/';
+    private $directory = '../app/static/documentation/';
 
     public function getListDocuments(){
         $files = scandir($this->directory);
